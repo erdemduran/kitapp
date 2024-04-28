613f33570a4edb1d13726d08d31a72bfb7c7258f
erdemduran <111234949+erdemduran@users.noreply.github.com>
2024-04-28 21:25:52 +0300

    Kitap.cs

---
+++ Kitap.cs
@@ -0,0 +1,12 @@
+namespace kitap1.Models
+{
+    public class Kitap
+    {
+        public string KitapAdı { get; set; }
+        public string YazarAdı { get; set; }
+        public DateTime YayınlanmaTarihi { get; set; }
+        public string  Türü { get; set; }
+        public int SayfaSayısı { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
